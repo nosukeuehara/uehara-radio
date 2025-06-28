adcefd49f2d35ade16eb7967c73b6a4af3d6fd24
DE-Uehara <r.uehara@defide-ix.com>
2025-06-28 15:55:53 +0900

    update : ページトップのコンポーネント作成

--- src/components/PageTop.tsx
+++ src/components/PageTop.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 const PageTop = () => {
   return (
     <div className="bg-blue-400 w-full h-screen flex items-center justify-center">
-      <div className="relative w-40 h-40 sm:w-60 sm:h-60 md:w-80 md:h-80 lg:w-[400px] lg:h-[400px]">
+      <div className="relative w-60 h-60 sm:w-60 sm:h-60 md:w-80 md:h-80 lg:w-[400px] lg:h-[400px]">
         <Image
           src="/toppageImg.png"
           alt="top page image"
           fill
           className="object-contain"
-          sizes="(min-width: 1024px) 400px, (min-width: 768px) 320px, (min-width: 640px) 240px, 160px"
+          sizes="(min-width: 1024px) 400px, (min-width: 768px) 320px, (min-width: 640px) 240px"
         />
       </div>
     </div>
