cbb9614a84eb9c7f6da1b478b20a764108450288
DE-Uehara <ryuunosuke06160616@gmail.com>
2025-12-12 13:48:57 +0900

    update : 軽微な修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div>
       <footer className="text-center text-sm text-gray-500 bg-cyan-50 py-6">
-        © 2025 ウエハレイディオ. All rights reserved.
+        © {new Date().getFullYear()} ウエハレイディオ. All rights reserved.
       </footer>
     </div>
   );
